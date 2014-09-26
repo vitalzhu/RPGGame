828256b40995466fb0e30d9de38739a1b85db4aa
Vital <Vitalzhu@gmail.com>
2014-09-26 22:33:33 +0800

    添加了原始资源
    模型资源和状态机脚本

---
+++ Assets/Script/AI/FSM.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FSM
+{
+
+	protected State curState;			//当前状态，状态切换将依赖于当前状态
+	protected State lastState;			//上一次状态
+
+	#region PUBLIC_PROPERTY
+	/// <summary>
+	/// 当前状态
+	/// </summary>
+	/// <value>The state of the current.</value>
+	public State CurrentState
+	{
+		get{return curState;}
+	}
+
+	public State LastState
+	{
+		get{return lastState;}
+	}
+	#endregion PUBLIC_PROPERTY
+	/// <summary>
+	/// 用于注册的状态列表
+	/// </summary>
+	public Dictionary<string,State> stateList = new Dictionary<string, State>(); 
+
+	/// <summary>
+	/// 添加状态
+	/// </summary>
+	public virtual void RegistState(string stateName,State state)
+	{
+		stateList.Add (stateName,state);
+	}
+
+	/// <summary>
+	/// 切换状态
+	/// </summary>
+	/// <param name="newState">New state.</param>
+	public virtual void SetState(State newState)
+	{
+		if(curState != newState)
+		{
+			lastState = curState;
+			curState = newState;
+		}
+	}
+
+	public void AddState(State state)
+	{
+		RemoveState (state);
+		stateList.Add (state.Name,state);
+	}
+
+	public void RemoveState(State state)
+	{
+		if (stateList.ContainsKey (state.Name)) 
+		{
+			stateList.Remove(state.Name);
+		}
+	}
+
+	public void OnUpdate()
+	{
+		if(curState!=null)
+		{
+			curState.OnStateStay();
+		}
+	}
+}
---
+++ Assets/Script/AI/State.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State 
+{
+
+	protected string name;
+
+	public string Name
+	{
+		get{return name;}
+		/*
+		set{
+			if(value !=null)
+			{
+				name = value;
+			}
+		}*/
+	}
+
+	/// <summary>
+	/// 当新的状态进入
+	/// </summary>
+	public virtual void OnStateEnter()
+	{
+
+	}
+
+	/// <summary>
+	/// 停留在当前状态
+	/// </summary>
+	public virtual void OnStateStay()
+	{
+
+	}
+
+	/// <summary>
+	/// 退出当前状态
+	/// </summary>
+	public virtual void OnStateExit()
+	{
+
+	}
+
+
+
+}
+
+public class StateName
+{
+	public static string Idle = "Idle";
+	public static string Run = "Run";
+	public static string Walk = "Walk";
+	public static string Damage = "Damage";
+	public static string Dead = "Dead";
+	public static string Attack = "Attack";
+	public static string ComboAttack = "ComboAttack";
+	public static string Skill = "Skill";
+}
