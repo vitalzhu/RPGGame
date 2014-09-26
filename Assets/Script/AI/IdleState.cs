57aafbfcf4bf7a528a68d8ea8d5b87c9ff459b21
Vital <Vitalzhu@gmail.com>
2014-09-26 22:59:10 +0800

    添加基础构架脚本
    删除测试脚本BehaviourController，添加了IdleState和AnimationController类，添加了基础类baseObjecr

---
+++ Assets/Script/AI/IdleState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleState : State 
+{
+	public IdleState()
+	{
+		this.name = StateName.Idle;
+	}
+
+    public override void OnStateEnter()
+    {
+        base.OnStateEnter();
+    }
+
+    public override void OnStateStay()
+    {
+        base.OnStateStay();
+    }
+    public override void OnStateExit()
+    {
+        base.OnStateExit();
+    }
+}
---
+++ Assets/Script/BaseObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseObject : MonoBehaviour {
+
+    public string name = "Object";
+    public string flag = null;
+}
